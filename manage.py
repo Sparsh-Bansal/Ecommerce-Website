e536f13df366686c0a48b13193b727fb06f16a4c
shubham5630994 <72184892+shubham5630994@users.noreply.github.com>
2020-10-01 12:08:30 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -11,7 +11,7 @@ def main():
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
+            "available on your PYTHONPATH environment variable? OOPs seems like you "
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
